d863d7a2dc1e7110b49f74220436a686199fd1cc
Saptarshi Guha <saptarshi.guha@gmail.com>
2016-01-14 13:27:56 -0800

    Works on Linux too

--- python-driver.py
+++ python-driver.py
@@ -8,18 +8,28 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 import calendar
 import time
+import os, sys
 
+## Change this.
+## numiter is 25 to match talos, though I have kept 50
+## filename is where the output is kept
+## Note here, firefox is started once and the page is reloaded multiple times
+## Another approach would be to restart Firefox each time
 
-binary = FirefoxBinary("/Applications/FirefoxNightly.app/Contents/MacOS/firefox")
-driver = webdriver.Firefox() #firefox_binary=binary)
-
-numiter = 25
+numiter = 1
 filename = "/tmp/glterrain-result-"+str(calendar.timegm(time.gmtime()))+".json"
+
+
+perffile="file://"+os.path.abspath("./perftest.html")
+
+#binary = FirefoxBinary("/Applications/FirefoxNightly.app/Contents/MacOS/firefox")
+
+driver = webdriver.Firefox() #firefox_binary=binary)
 f = open(filename, "w")
 try:
     for x in range(0,numiter):
         print(str(x)+" trial")
-        driver.get("file:///Users/sguha/mz/misc/glterrain-runner/perftest.html")
+        driver.get(perffile)
         WebDriverWait(driver, 30*60).until(EC.title_contains("DONE"))
     script = 'return localStorage.getItem("glTerrainJSONData") ;'
     result = driver.execute_script(script)
